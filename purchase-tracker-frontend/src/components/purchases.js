009c468982f89869485f4a22217d53966b74fd49
jon <jonathan.wong110@gmail.com>
2020-05-11 15:35:33 -0400

    created a function to bind the input fields

--- purchase-tracker-frontend/src/components/purchases.js
+++ purchase-tracker-frontend/src/components/purchases.js
@@ -2,7 +2,17 @@ class Purchases {
     constructor() {
         this.purchases = []
         this.adapter = new PurchasesAdapter()
+        this.initiBindingsAndEventListeners()
         this.fetchAndLoadPurchases()
+    }
+
+    initiBindingsAndEventListeners() {
+        this.purchasesContainer = document.getElementById('purchases-container')
+        this.newPurchaseTitle = document.getElementById('new-purchase-title')
+        this.newPurchasePrice = document.getElementById('new-purchase-price')
+        this.newPurchaseDescription = document.getElementById('new-purchase-description')
+        this.newPurchaseImage = document.getElementById('new-purchase-image')
+        this.purchaseForm = document.getElementById('new-purchase-form')
     }
 
     fetchAndLoadPurchases() {
