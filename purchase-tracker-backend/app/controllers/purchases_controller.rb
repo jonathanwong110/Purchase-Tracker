c621e60107fd25e8b188f42f10e1e896bf0b3647
jon <jonathan.wong110@gmail.com>
2020-05-11 14:26:32 -0400

    created backend portion and a controller for purchases, and uncommented the cors file and rack-cors gem

---
+++ purchase-tracker-backend/app/controllers/purchases_controller.rb
@@ -0,0 +1,44 @@
+class PurchasesController < ApplicationController
+  before_action :set_purchase, only: [:show, :update, :destroy]
+
+  def index
+    @purchases = Purchase.all
+
+    render json: @purchases
+  end
+
+  def show
+    render json: @purchase
+  end
+
+  def create
+    @purchase = Purchase.new(purchase_params)
+
+    if @purchase.save
+      render json: @purchase, status: :created, location: @purchase
+    else
+      render json: @purchase.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @purchase.update(purchase_params)
+      render json: @purchase
+    else
+      render json: @purchase.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @purchase.destroy
+  end
+
+  private
+    def set_purchase
+      @purchase = Purchase.find(params[:id])
+    end
+
+    def purchase_params
+      params.require(:purchase).permit(:title, :price, :description, :image)
+    end
+end
---
+++ purchase-tracker-backend/db/migrate/20200511182442_create_purchases.rb
@@ -0,0 +1,12 @@
+class CreatePurchases < ActiveRecord::Migration[6.0]
+  def change
+    create_table :purchases do |t|
+      t.string :title
+      t.string :price
+      t.string :description
+      t.string :image
+
+      t.timestamps
+    end
+  end
+end
