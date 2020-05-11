f68ee7076949ef9e14f64af7c323a84c6a565fd2
jon <jonathan.wong110@gmail.com>
2020-05-11 15:55:59 -0400

    made it so purchases can now be rendered, text lines are now clickable, made some slight css changes

--- purchase-tracker-frontend/src/adapters/PurchasesAdapter.js
+++ purchase-tracker-frontend/src/adapters/PurchasesAdapter.js
@@ -7,4 +7,20 @@ class PurchasesAdapter {
         return fetch(this.baseUrl).then(res => res.json())
     }
 
+    createPurchase(titleValue, priceValue, descriptionValue, imageValue) {
+        const purchase = {
+            title: titleValue,
+            price: priceValue,
+            description: descriptionValue,
+            image: imageValue
+        }
+        return fetch(this.baseUrl, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json',
+            },
+            body: JSON.stringify({ purchase }),
+        }).then(res => res.json())
+    }
+
 }
--- purchase-tracker-frontend/src/components/purchases.js
+++ purchase-tracker-frontend/src/components/purchases.js
@@ -13,12 +13,48 @@ class Purchases {
         this.newPurchaseDescription = document.getElementById('new-purchase-description')
         this.newPurchaseImage = document.getElementById('new-purchase-image')
         this.purchaseForm = document.getElementById('new-purchase-form')
+        this.purchaseForm = document.getElementById('new-purchase-form')
+        this.purchaseForm.addEventListener('submit', this.createPurchase.bind(this))
+        this.purchasesContainer.addEventListener('dblclick', this.handlePurchaseClick.bind(this))
+    }
+
+    handlePurchaseClick(e) {
+        const card = e.target
+        if (card.attributes && card.attributes.class && card.attributes.class.value === "selectable") {
+            card.contentEditable = true
+            card.focus()
+            card.classList.add('editable')
+        }
+    }
+
+    createPurchase(e) {
+        e.preventDefault()
+        const titleValue = this.newPurchaseTitle.value
+        const priceValue = this.newPurchasePrice.value
+        const descriptionValue = this.newPurchaseDescription.value
+        const imageValue = this.newPurchaseImage.value
+        this.adapter.createPurchase(titleValue, priceValue, descriptionValue, imageValue).then(purchase => {
+            this.purchases.push(new Purchase(purchase))
+            this.newPurchaseTitle.value = ""
+            this.newPurchasePrice.value = ""
+            this.newPurchaseDescription.value = ""
+            this.newPurchaseImage.value = ""
+            this.render()
+        })
     }
 
     fetchAndLoadPurchases() {
         this.adapter.getPurchases().then(purchases => {
-            console.log(purchases)
+            purchases.forEach(purchase => this.purchases.push(new Purchase(purchase)))
         })
+        .then(() => {
+            this.render()
+        })
+    }
+
+
+    render() {
+        this.purchasesContainer.innerHTML = this.purchases.map(purchase => purchase.renderCard()).join('')
     }
 
 }
